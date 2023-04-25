e97e30ea09ed330fdba0cc1dbfa6a1fa715579ea
Matheus Pelucio <mts.rangel@hotmail.com>
2023-04-24 22:19:39 -0300

    Ajustes nos textos

--- src/components/Mission/index.js
+++ src/components/Mission/index.js
@@ -11,10 +11,10 @@ export default function Mission() {
           <img src={CircleMash} alt='Malha 3D circular' />
           <div className='text flex'>
             <p>
-              Nosso principal objetivo é oferecer soluções integradas e personalizadas para atender às necessidades específicas de cada cliente. Com um trabalho colaborativo e multidisciplinar, envolvendo profissionais especializados.
+              Oferecer soluções integradas e personalizadas para atender às necessidades específicas de cada cliente. Com um trabalho colaborativo e multidisciplinar, envolvendo profissionais especializados.
             </p>
             <p>
-              Entre os principais benefícios oferecidos por uma empresa desse tipo estão a melhoria da eficiência produtiva, a redução de custos e o aumento da produtividade no mercado. Além disso, a empresa pode contribuir para o desenvolvimento de novas tecnologias e soluções que podem beneficiar a sociedade como um todo.
+              Entre os principais benefícios oferecidos estão a melhoria da eficiência produtiva, a redução de custos e o aumento da produtividade no mercado. Além disso, a empresa pode contribuir para o desenvolvimento de novas tecnologias e soluções que podem beneficiar a sociedade como um todo.
             </p>
             <p>
               Em resumo, uma empresa que tem como objetivo solucionar problemas utilizando engenharia mecânica e programação busca aplicar o conhecimento técnico e científico para oferecer soluções inovadoras e eficientes para desafios complexos em diferentes setores.
